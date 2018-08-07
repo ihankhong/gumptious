2b48906b88c93e0d516c89e9319eeb0f368bea19
iHank <ihank@jetfuel.com.tw>
2018-08-08 00:38:29 +0800

    css, js plugin and topbar finish

---
+++ header.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>鼎億自動化股份有限公司</title>
+
+	<!-- jQuery	 -->
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<!-- bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<!-- animate -->
+	<link href="css/animate.css" rel="stylesheet">
+	<!-- font-awesome -->
+	<link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<!-- custom -->
+	<link href="css/nav.css" rel="stylesheet">
+	<!-- <link href="css/app.css" rel="stylesheet"> -->
+	<!-- <script src="js/app.js"></script> -->
+</head>
+<body>
+<div id="top">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5">
+				<a href="#"><img src="image/logo.png" class="img-fluid" alt="LOGO"></a>
+			</div>
+			<div class="col-md-7">
+				<div class="d-flex">
+					<ul class="nav mr-auto">
+						<li class="nav-item">
+							<a class="nav-link active" href="#"><img src="image/facebook.png" class="img-fluid" width="32" alt="Facebook"></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#"><img src="image/youtube.png" class="img-fluid" width="32" alt="YouTube"></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#"><img src="image/google-plus.png" class="img-fluid" width="32" alt="Google Plus"></a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">繁體中文</a>
+							<div class="dropdown-menu">
+								<a class="dropdown-item" href="#">繁體中文</a>
+								<a class="dropdown-item" href="#">English</a>
+							</div>
+						</li>
+					</ul>
+					<form class="form-inline">
+						<label for="search" class="mr-sm-2">搜尋</label>
+						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+						<img src="image/search.png" class="img-fluid search-btn" alt="Search">
+						<!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
