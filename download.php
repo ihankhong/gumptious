6b81541ce00f0f57aa6bcc1d3947d1caf0dc898a
iHank <ihank@jetfuel.com.tw>
2018-08-11 16:09:54 +0800

    download

---
+++ download.php
@@ -0,0 +1,140 @@
+<?php
+require_once('layout/header.php');
+
+$breadcrumb = ['型錄下載','GNP系列'];
+require_once('layout/breadcrumb.php');
+
+$title = ['title' => 'GNP系列', 'subtitle' => 'Download'];
+require_once('layout/title.php');
+
+$sidebar = [
+    'title' => '下載專區',
+    'items' => ['型錄','GNP系列','NT系列','WS系列','GNDU系列'],
+    'active' => 2
+];
+require_once('layout/wrapper.php');
+?>
+<h3>適用馬達瓦數</h3>
+<div class="card card-download">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-top-border-none justify-content-center">
+                <thead>
+                    <tr>
+                        <th scope="col">50-100W</th>
+                        <th scope="col">下載</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">GNP40L1-8-30-46-M4</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP40L2-8-30-46-M4</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-top-border-none">
+                <thead>
+                    <tr>
+                        <th scope="col">200-400W</th>
+                        <th scope="col">下載</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">GNP60L1-14-50-70-M5(出力軸14)</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP60L2-14-50-70-M5(出力軸14)</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP60L1-14-50-70-M5(出力軸16)</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP60L2-14-50-70-M5(出力軸16)</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-top-border-none">
+                <thead>
+                    <tr>
+                        <th scope="col">750W</th>
+                        <th scope="col">下載</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">GNP90L1-19-70-90-M6</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP90L2-19-70-90-M6</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP90-3-19-70-90-M6</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP90-(9,12,15,21,30)-19-70-90-M6</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-top-border-none">
+                <thead>
+                    <tr>
+                        <th scope="col">1KW以上</th>
+                        <th scope="col">下載</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">GNP120L1-22-95-115-M8</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP120L2-22-95-115-M8</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP120L2-24-110-145-M8</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP150L1-24-110-145-M8</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP150L2-24-110-145-M8</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP150L1-35-114.3-200-M12</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">GNP150L2-35-114.3-200-M12</td>
+                        <td><a class="btn btn-link p-0" href="#" role="button"><img src="image/download-btn.png" class="img-fluid" width="100" alt="Download Btn"></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php
+require_once('layout/footer.php');
+?>
