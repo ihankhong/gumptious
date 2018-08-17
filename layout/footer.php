08b4b4dda32f38adfab5c70b5f4918f383a56e36
iHank <ihank@jetfuel.com.tw>
2018-08-17 22:09:28 +0800

    modify footer

--- layout/footer.php
+++ layout/footer.php
@@ -7,7 +7,7 @@
                 <p class="mt-3">鼎億有限公司專業從事減速機的研發、生產、設計，是精密機械動作元件的供應者，我們擁有豐富的經驗協助客戶將減速機導入機械產品中。</p>
             </div>
             <div class="col-md-4">
-                <h4>MENU</h4>
+                <h4 id="menu-title" class="title"><span>MENU</span></h4>
                 <ul class="nav flex-column">
                     <li class="nav-item">
                         <a class="nav-link pl-0 active" href="company.php">關於鼎億</a>
@@ -24,7 +24,7 @@
                 </ul>
             </div>
             <div class="col-md-4">
-                <h4>CONTACT US</h4>
+                <h4 id="contactus-title" class="title"><span>CONTACT US</span></h4>
                 <p class="mt-3 mb-1">鼎億自動化股份有限公司(台中總公司)</p>
                 <p class="mb-1">Gumptious Technology Co.,</p>
                 <p class="mb-1">電話：<a href="tel:04-24919100">04-24919100</a></p>
