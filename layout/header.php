4e69165ee2435c1cbdbf8d93c61f449b62d56edb
iHank <ihank@jetfuel.com.tw>
2018-08-17 21:43:38 +0800

    navbar mobile 調整好

--- layout/header.php
+++ layout/header.php
@@ -49,7 +49,7 @@ $nav = [
 				<div class="d-sm-flex d-block flex-sm-nowrap">
 					<ul class="nav ml-auto">
 						<li class="nav-item">
-							<a class="nav-link active" href="#"><img src="image/facebook.png" class="img-fluid" width="32" alt="Facebook"></a>
+							<a class="nav-link" href="#"><img src="image/facebook.png" class="img-fluid" width="32" alt="Facebook"></a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#"><img src="image/youtube.png" class="img-fluid" width="32" alt="YouTube"></a>
@@ -68,7 +68,6 @@ $nav = [
 					<form class="form-inline">
 						<label for="search" class="mx-sm-2">搜尋</label>
 						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-						<!-- <a href="#"><img src="image/search.png" class="img-fluid search-btn" alt="Search"></a> -->
 						<button class="btn btn-link p-0" type="submit"><img src="image/search.png" class="img-fluid search-btn" alt="Search"></button>
 					</form>
 				</div>
@@ -79,18 +78,42 @@ $nav = [
 <!-- topbar end -->
 <!-- navbar start -->
 <nav class="menu navbar navbar-expand-lg navbar-dark" role="navigation">
-  	<div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
-		<!-- <a class="navbar-brand" href="#">Navbar</a> -->
+  	<div class="container">
+		<a class="navbar-brand mb-1" href="index.php"><img src="image/logo-small.png" class="img-fluid" width="54" alt="LOGO Small"></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarNavDropdown">
-			<ul class="navbar-nav">
+		<div class="navbar-collapse collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav mx-auto">
 				<?php foreach ($nav as $key => $item): ?>
 					<li class="nav-item<?php echo $key+1 === $active ? ' active' : ''; ?>">
 						<a class="nav-link" href="<?php echo $item['link']; ?>"><?php echo $item['title']; ?></a>
 					</li>
 				<?php endforeach; ?>
+			</ul>
+			<ul class="navbar-nav topNav">
+				<li class="nav-item">
+					<div class="d-flex justify-content-center">
+						<a class="nav-link mr-2" href="#"><img src="image/facebook.png" class="img-fluid" width="32" alt="Facebook"></a>
+						<a class="nav-link mr-2" href="#"><img src="image/youtube.png" class="img-fluid" width="32" alt="YouTube"></a>
+						<a class="nav-link" href="#"><img src="image/google-plus.png" class="img-fluid" width="32" alt="Google Plus"></a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle mt-1" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">繁體中文</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="#">简体中文</a>
+						<a class="dropdown-item" href="#">ENGLISH</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<form class="searchWrapper mt-2">
+						<div class="form-group">
+							<input class="form-control" type="search" placeholder="Search" aria-label="Search">
+							<button class="btn btn-block search-btn mt-2" type="submit">搜尋<i class="fa fa-search ml-2" aria-hidden="true"></i></button>
+						</div>
+					</form>
+				</li>
 			</ul>
 		</div>
 	</div>
