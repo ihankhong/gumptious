ff0d6c72a1d4e671bf6fcdf22f89ad03d71db2f9
iHank <ihank@jetfuel.com.tw>
2018-10-06 20:41:15 +0800

    step2 finish

---
+++ unique/step2.php
@@ -0,0 +1,90 @@
+<form action="../unique_service.php?step=3" method="post" class="needs-validation us-step2" novalidate>
+    <h3>載具資訊</h3>
+    <div class="form-row">
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique01">1. 附載重量(KG)</label>
+            <input type="text" class="form-control" id="validationUnique01" placeholder="KG" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique02">2. 導距/節距(MM)</label>
+            <input type="text" class="form-control" id="validationUnique02" placeholder="MM" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique03">3. 螺桿長(MM)</label>
+            <input type="text" class="form-control" id="validationUnique03" placeholder="MM" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique04">4. 螺桿直徑(MM)</label>
+            <input type="text" class="form-control" id="validationUnique04" placeholder="MM" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique05">5. 上升速度(MM/MIN)</label>
+            <input type="text" class="form-control" id="validationUnique05" placeholder="MM/MIN" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique06">6. 選擇馬達(MM)</label>
+            <input type="text" class="form-control" id="validationUnique06" placeholder="MM" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique07">7. 摩擦係數(MM)</label>
+            <input type="text" class="form-control" id="validationUnique07" placeholder="MM" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique08">8. 加速/減速時間(MM/MIN)</label>
+            <input type="text" class="form-control" id="validationUnique08" placeholder="MM/MIN" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique09">9. 選擇馬達</label>
+            <select class="form-control" id="validationUnique09" required>
+                <option value="">-----請選擇-----</option>
+                <option value="100W">100W</option>
+                <option value="400W">400W</option>
+                <option value="750W">750W</option>
+                <option value="1KW">1KW</option>
+                <option value="2KW">2KW</option>
+                <option value="2KW">2KW以上</option>
+            </select>
+            <div class="invalid-feedback">請選擇一個</div>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-4 mt-3">
+            <label for="validationUnique10">10. 電機額定轉速(RPM)</label>
+            <input type="text" class="form-control" id="validationUnique10" placeholder="RPM" required>
+            <div class="invalid-feedback">請輸入數值</div>
+        </div>
+    </div>
+    <div class="btns text-right mt-3">
+        <button type="submit" class="btn bg-submit px-5" role="button">下一步 <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button>
+    </div>
+</form>
+<script>
+    (function() {
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+</script>
--- unique_service.php
+++ unique_service.php
@@ -22,60 +22,9 @@ $step = isset($_GET['step']) ? $_GET['step'] : 1;
         <div class="content">
             <img src="image/page/uniqueservice/step<?php echo $step?>.png" class="img-fluid" alt="Unique Service Step">
             <div class="uniqueServiceStepWrapper">
-                <form>
-                    <h3>運動方向</h3>
-                    <div class="form-row">
-                        <div class="col-sm-6">
-                            <select name="items" class="form-control">
-                                <option value="">-----請選擇-----</option>
-                                <option value="1">螺桿轉矩</option>
-                                <option value="2">齒輪傳動</option>
-                                <option value="3">線性運動</option>
-                                <option value="4">水平旋轉</option>
-                                <option value="5">垂直旋轉</option>
-                            </select>
-                        </div>
-                    </div>
-                    <!-- 適用系列區塊 -->
-                    <div class="applicable-area">
-                        <h3>適用系列</h3>
-                        <p>您可能適用下列幾項系列，請點選「下一步」更精準計算出與您數據匹配的機種</p>
-                        <div class="form-row">
-                            <div class="col-md-4">
-                                <img src="image/page/uniqueservice/items/1.png" class="img-fluid mx-auto d-block"
-                                     alt="Unique Service
-                                 Item 1">
-                                <p class="item-title text-center">適用系列1</p>
-                            </div>
-                            <div class="col-md-4">
-                                <img src="image/page/uniqueservice/items/2.png" class="img-fluid mx-auto d-block" alt="Unique Service
-                                 Item 2">
-                                <p class="item-title text-center">適用系列2</p>
-                            </div>
-                            <div class="col-md-4">
-                                <img src="image/page/uniqueservice/items/3.png" class="img-fluid mx-auto d-block" alt="Unique Service
-                                 Item 3">
-                                <p class="item-title text-center">適用系列3</p>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="col-md-4">
-                                <img src="image/page/uniqueservice/items/4.png" class="img-fluid mx-auto d-block"
-                                     alt="Unique Service Item 4">
-                                <p class="item-title text-center">適用系列4</p>
-                            </div>
-                            <div class="col-md-4">
-                                <img src="image/page/uniqueservice/items/5.png" class="img-fluid mx-auto d-block"
-                                     alt="Unique Service Item 5">
-                                <p class="item-title text-center">適用系列5</p>
-                            </div>
-                        </div>
-                        <div class="btns text-right mt-3">
-                            <button type="submit" class="btn bg-submit px-5" role="button">下一步 <i class="fa
-                            fa-arrow-circle-o-right" aria-hidden="true"></i></button>
-                        </div>
-                    </div>
-                </form>
+                <?php
+                require_once('unique/step' . $step . '.php');
+                ?>
             </div>
         </div>
         <!-- content end -->
@@ -83,18 +32,6 @@ $step = isset($_GET['step']) ? $_GET['step'] : 1;
     <!-- content wrapper end -->
 </article>
 <!-- article end -->
-<script>
-$(function(){
-    $('select[name=items]').change(function (event) {
-        if (this.value === '') {
-            $('.applicable-area').removeClass('show');
-            return false;
-        }
-
-        $('.applicable-area').addClass('show');
-    });
-});
-</script>
 <?php
 require_once('layout/footer.php');
 ?>
